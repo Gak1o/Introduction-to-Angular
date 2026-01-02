9d3072b9293ed4e102c1dfea9b0d2cadf25c949d
Gak1o <gakiotoni@gmail.com>
2026-01-02 14:28:44 +0300

    introduction to Components and Data binding

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,25 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Home } from './home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, Header],
   template: `
-    <h1>Welcome to {{ title() }}!</h1>
+    <app-header/>
+    <main>
+      <app-home/>
+    </main>
 
-    <router-outlet />
   `,
-  styles: [],
+  styles: [ `
+  main {
+   padding: 16px;
+
+  }
+`
+  ],
 })
 export class App {
   protected readonly title = signal('first-ng-app');
---
+++ src/app/home/home.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Greeting } from '../components/greeting/greeting';
+
+@Component({
+  selector: 'app-home',
+  imports: [Greeting],
+  templateUrl: './home.html',
+  styleUrl: './home.scss',
+})
+export class Home {
+  message='Hujambo dunia!';
+
+}
